21e9f56121b13cdfeed5f3939fdec31a519a1c5b
sanjarbek <sanjarsaidov173@gamil.com>
2022-07-04 20:08:43 +0500

    done

---
+++ lesson15/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Sanjarbek ID card"),
+          backgroundColor: Colors.grey[850],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Center(
+                child: CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage("asserts/image.jpg"),
+                ),
+              ),
+              const Text(
+                "name",
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              Text(
+                "Sanjarbek",
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              const Text(
+                "current ninja level",
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              Text(
+                "8",
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "sanjarsaidov173@gmail.com",
+                    style: TextStyle(color: Colors.grey[400]),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
