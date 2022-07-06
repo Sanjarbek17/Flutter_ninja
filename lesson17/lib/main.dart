d61f0f0ba477486bef476536ca939390825dc6c3
sanjarbek <sanjarsaidov173@gamil.com>
2022-07-06 11:13:07 +0500

    done

---
+++ lesson17/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> lst = ['aefadsfaefa', 'asdfefadfa', 'fa;ewifa;dshja;dfjkadsf'];
+  int index = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // ignore: avoid_print
+          print("ishladi");
+          setState(() {});
+          index++;
+          if (index == 3) {
+            index = 0;
+          }
+        },
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text("list data"),
+      ),
+      body: Center(child: Text(lst[index])),
+    );
+  }
+}
